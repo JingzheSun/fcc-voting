97e3e0f8705e6354f7d94b087f1032dda411f66a
Haha <jis252@eng.ucsd.edu>
2017-09-01 01:06:11 -0700

    fixed

--- public/auth.js
+++ public/auth.js
@@ -2,8 +2,8 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'      : 'fphtzKSjvC8T0FhDoPm4aHNCO', // your App ID
         'consumerSecret'  : 'KjWwDTmImp5pkphrT0wfHKOXcX1ALjTOfUWXpZgbpQg7o25hrS', // your App Secret
-        //'callbackURL'   : 'https://fcc-vote-appjs.herokuapp.com/'
-        'callbackURL'   : 'http://127.0.0.1:8070/login/callback'
+        'callbackURL'   : 'https://fcc-vote-appjs.herokuapp.com/login/callback'
+        //'callbackURL'   : 'http://127.0.0.1:8070/login/callback'
     },
 
     'facebookAuth' : {
